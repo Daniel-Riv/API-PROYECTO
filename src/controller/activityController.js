a4686e319061139ead014eda18586f5483640a9c
ArgeNH <arge.nino@uptc.edu.co>
2022-11-12 16:28:33 -0500

    error

--- src/controller/activityController.js
+++ src/controller/activityController.js
@@ -2,25 +2,26 @@ const activitySchema = require('../models/activity');
 const matterSchema = require('../models/matter');
 
 const createActivity = async (req, res) => {
-    const {name, description, dateStart, dateEnd, gradeActivity,idMatter} = req.body;
+    const { name, description, dateStart, dateEnd, gradeActivity, idMatter } = req.body;
     try {
-        const valdiaMatter = await matterSchema.findById({_id: idMatter});
-        if(!valdiaMatter){
+        const valdiaMatter = await matterSchema.findById({ _id: idMatter });
+        if (!valdiaMatter) {
             return res.status(400).json({
                 success: false,
                 message: 'Matter is required'
             });
         }
-        const newActivity = new activitySchema({name, description, dateStart, dateEnd, gradeActivity,matter: idMatter});
+        const newActivity = new activitySchema({ name, description, dateStart, dateEnd, gradeActivity, matter: idMatter });
         await newActivity.save();
         return res.status(200).json({
             success: true,
-            message: 'Activity created', 
-            newActivity});
+            message: 'Activity created',
+            newActivity
+        });
     } catch (error) {
         return res.status(500).json({
             success: false,
-            message: 'Error to create activity', 
+            message: 'Error to create activity',
             error
         });
     }
@@ -44,9 +45,9 @@ const getActivities = async (req, res) => {
 }
 
 const getActivity = async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     try {
-        const activity = await activitySchema.findById({_id: id});
+        const activity = await activitySchema.findById({ _id: id });
         return res.status(200).json({
             success: true,
             message: 'Activity found',
@@ -62,17 +63,17 @@ const getActivity = async (req, res) => {
 }
 
 const updateActivity = async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     try {
-        const activity = await activitySchema.findById({_id: id},req.body,{new: true});
+        const activity = await activitySchema.findById({ _id: id }, req.body, { new: true });
         return res.status(200).json({
             success: true,
             message: 'Activity updated',
             activity
         });
-        } catch (error) {
-            return res.status(500).json({
+    } catch (error) {
+        return res.status(500).json({
             success: false,
             message: 'Error to update activity',
             error
@@ -82,9 +83,9 @@ const updateActivity = async (req, res) => {
 }
 
 const deleteActivity = async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     try {
-        const delet = await activitySchema.findByIdAndDelete({_id: id});
+        const delet = await activitySchema.findByIdAndDelete({ _id: id });
         return res.status(200).json({
             success: true,
             message: 'Activity deleted',
@@ -100,25 +101,26 @@ const deleteActivity = async (req, res) => {
 }
 
 const dateExpired = async (req, res) => {
-    const {id} = req.params;
-    try{
+    const { id } = req.params;
+    try {
         const date = new Date();
-        const activities = await activitySchema.findById({_id: id});
+        const activities = await activitySchema.findById({ _id: id });
         console.log(activities);
-        if(!activities){
+        if (!activities) {
             return res.status(400).json({
                 success: false,
                 message: 'Activity not found'
             });
         }
-           const subtract = date - activities.dateEnd;
+        const subtract = date - activities.dateEnd;
         return res.status(200).json({
             success: true,
             message: 'Date expired',
             subtract
         });
 
-    }catch(error){
+    } catch (error) {
+        console.log(error);
         return res.status(500).json({
             success: false,
             message: 'Error to get date expired',
